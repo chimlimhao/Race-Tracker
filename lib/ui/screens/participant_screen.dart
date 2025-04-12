5b90e5d2e1e42fc335b0e22ca08827a5346459b0
limhao2705 <limhao2705@gmail.com>
2025-04-12 21:00:19 +0700

    RT-001: Implemented Bottom Navigation Logic

---
+++ lib/ui/screens/participant_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ParticipantScreen extends StatelessWidget {
+  const ParticipantScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Participant')));
+  }
+}
