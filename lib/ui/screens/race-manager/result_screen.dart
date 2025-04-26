5b3c539e66778ed0426c8747db10ee9c615e0516
limhao2705 <limhao2705@gmail.com>
2025-04-26 15:11:15 +0700

    RT-006: Implemented the result screen.

--- lib/ui/screens/race-manager/result_screen.dart
+++ lib/ui/screens/race-manager/result_screen.dart
@@ -1,10 +1,160 @@
 import 'package:flutter/material.dart';
+import 'package:race_tracker/ui/screens/widgets/dialogs/search_input.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Result')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Result')),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 16, bottom: 8),
+            child: SearchInput(),
+          ),
+          Expanded(child: _resultList(context)),
+        ],
+      ),
+    );
+  }
+
+  Widget _resultList(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: Row(
+            children: [
+              Text(
+                'Participants (x):',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(width: 50),
+              SizedBox(width: 12),
+              Expanded(
+                flex: 3,
+                child: Text(
+                  'Name',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Text(
+                  'Gender',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'Time',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Divider(
+          height: 16,
+          indent: 24,
+          endIndent: 24,
+          color: Color(0xffb3b3b3),
+        ),
+        Expanded(
+          child: ListView.separated(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            itemCount: 6,
+            separatorBuilder:
+                (context, index) => Divider(color: Color(0xffe9e9e9)),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.symmetric(),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Color(0xffDAF1FF),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '1st',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 12),
+                    Expanded(
+                      flex: 3,
+                      child: Row(
+                        children: [
+                          Text('Limhao', style: TextStyle(fontSize: 16)),
+                          SizedBox(width: 8),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 4,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black),
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Text(
+                              '1001',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Text(
+                        'Male',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text('00:00:00', style: TextStyle(fontSize: 16)),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
